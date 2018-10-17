4abe1e9631c9287f7c8a2cd16ce9047ec084873b
tangchen2018 <tangchen2018@hotmail.com>
2018-10-17 16:16:42 +0800

    init

--- apps/utils.py
+++ apps/utils.py
@@ -3,13 +3,15 @@ from rest_framework import (viewsets)
 from apps.public.task.api import task1
 
 class GenericViewSetCustom(viewsets.ViewSet):
-    from apscheduler.schedulers.background import BackgroundScheduler
-    scheduler = BackgroundScheduler()
-    scheduler.start()
+    # from apscheduler.schedulers.background import BackgroundScheduler
+    # scheduler = BackgroundScheduler()
+    # scheduler.start()
 
     # def test():
     #     print("11111111111111")
     #
     # scheduler.add_job(test, 'interval', seconds=5)
 
-    scheduler.add_job(task1,'interval',minutes=1 )
+    # scheduler.add_job(task1,'interval',minutes=1 )
+
+    pass
